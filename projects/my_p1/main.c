aeea6cf66c6f1242daf39e8dc32acaf11ea5490f
BillXSY <714017703@qq.com>
2021-10-11 02:01:02 +0800

    1011

---
+++ projects/my_p1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+int main(int argc, char **argv) {
+
+}
