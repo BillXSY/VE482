107d97fc022562dd7b84584a6d9a1d354aec307c
BillXSY <714017703@qq.com>
2021-10-11 20:30:17 +0800

    Update main.c

--- projects/my_p1/main.c
+++ projects/my_p1/main.c
@@ -1,14 +1,18 @@
 #include "util.h"
 
+
 int main() {
 
 
     while (1) {
+
         prompt("mumsh $ ");
 
+        // initialize processed input
         char *input_processed = (char *) malloc(sizeof(char) * MAX_LINE);
         memset(input_processed, 0, MAX_LINE);
 
+        //get input and process
         get_input(input_processed);
 
         if (!strcmp(input_processed, "exit")) {
@@ -23,11 +27,24 @@ int main() {
         int argv_length = split_input(input_processed, argv);
 
         redirect_t tmp_re;
+        tmp_re.in = 0;
+        tmp_re.out = 0;
+        for (int j = 0; j < argv_length; ++j) {
+            if (!strcmp(argv[j], "<")) {
+                tmp_re.in = REDIRECT_IN;
+                tmp_re.inFile = argv[j + 1];
+            }
+            if (!strcmp(argv[j], ">")) {
+                tmp_re.out = REDIRECT_OUT;
+                tmp_re.outFile = argv[j + 1];
+            }
+            if (!strcmp(argv[j], ">>")) {
+                tmp_re.out = REDIRECT_APPEND;
+                tmp_re.outFile = argv[j + 1];
+            }
+        }
 
-
-
-
-        check_argv(argv,argv_length);
+//        check_argv(argv,argv_length);
 
 
         break;
