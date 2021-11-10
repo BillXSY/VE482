bb529b7c6da0746d01364a8cf3c12f85581141c6
BillXSY <714017703@qq.com>
2021-11-10 22:09:48 +0800

    1110

--- labs/lab6/code/dl_sort_txt/dl_sort_txt.c
+++ labs/lab6/code/dl_sort_txt/dl_sort_txt.c
@@ -6,15 +6,8 @@
 void sort_txt(char* argv1, char *argv2) {
     srand((unsigned) time(NULL));
 
-/*    if (argc != 3) {
-        printf("Missing arguments!\n");
-        return -1;
-    }*/
-
-
     Mode mode;
     getType(&mode, argv1, argv2);
-
 
     // read the input file
     List *list = txt_in(&mode, argv1);
@@ -23,7 +16,6 @@ void sort_txt(char* argv1, char *argv2) {
     // checkList(mode, &list);
     sort(&mode, list);
     // checkList(mode, &list);
-
 
     // output
     to_txt(&mode, list);
---
+++ labs/lab6/lab6.md
@@ -0,0 +1,15 @@
+# VE482 — Introduction to Operating Systems Lab 6
+
+[TOC]
+
+## 1. A real life software
+
+Based on Zathura plugin documentation and Zathura-cb:
+
+- Write the skeleton of a simple Zathura plugin allowing to open a text file. 
+- Complete the plugin such that Zathura can open and display text files.
+
+## 2. Designing a plugin architecture
+
+Check `code/` for details
+
